32436074f064df3667aac0036808671b86c777d9
Matthew Hood <matthewhood@Matthews-MBP-3.lan>
2022-05-06 14:54:13 -0400

    initial commit nes website

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,55 @@
 import * as React from "react"
-import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
+import * as styles from './style.module.scss';
 
-import Layout from "../components/layout"
+import Button from "../components/atom/Button/button"
+import Card from "../components/Card/card"
+
+
+import Layout from "../components/Layout/layout"
 import Seo from "../components/seo"
+import IntroText from "../components/IntroText/introText"
+import BreakerLarge from "../components/BreakerLarge/breakerLarge"
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-      <Link to="/using-dsg">Go to "Using DSG"</Link>
-    </p>
+<BreakerLarge isHero={true}>
+  <div className={styles.centerGatsbyImage}>
+  <StaticImage
+  src="../images/wave.png"
+  width={300}
+  height={500}
+  quality={95}
+  formats={["auto", "webp", "avif"]}
+  alt="A Gatsby astronaut"
+  style={{  filter: "drop-shadow(30px 10px 4px black)", transform: "scale(.5)", marginTop: "-4rem" }}
+/>
+  </div>
+  <IntroText 
+  titleH1="Digital Agency" 
+  linkSrc='/' 
+  body='Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.'
+  linkText='Contact Us'
+  componentPosition='center'
+  buttonTopRight={false}
+  />
+  <Card 
+  title='Feature Concepts'
+  body='Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.'
+  />
+  <Card 
+  title='Big Ideas'
+  body='Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.'
+  />
+    <Card 
+  title='Creative'
+  body='Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.'
+  />
+
+</BreakerLarge>
+<Button to='/' primary={true} text="Contact Us"/>
+  
   </Layout>
 )
 

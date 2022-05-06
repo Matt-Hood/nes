344cb458cda88cd185f1a8f1cf4e9d29cb27d960
Matthew Hood <matthewhood@Matthews-MBP-3.lan>
2022-05-06 17:51:44 -0400

    adding base components for home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,11 +10,15 @@ import Layout from "../components/Layout/layout"
 import Seo from "../components/seo"
 import IntroText from "../components/IntroText/introText"
 import BreakerLarge from "../components/BreakerLarge/breakerLarge"
+import Breaker from "../components/Breaker/breaker"
+import CardList from "../components/CardList/cardList"
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-<BreakerLarge isHero={true}>
+
+    {/* Hero  Start*/}
+<BreakerLarge isHero={true} background={'#160f20'}>
   <div className={styles.centerGatsbyImage}>
   <StaticImage
   src="../images/wave.png"
@@ -34,6 +38,7 @@ const IndexPage = () => (
   componentPosition='center'
   buttonTopRight={false}
   />
+  <CardList>
   <Card 
   title='Feature Concepts'
   body='Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.'
@@ -47,8 +52,167 @@ const IndexPage = () => (
   body='Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.'
   />
 
+  </CardList>
+
 </BreakerLarge>
-<Button to='/' primary={true} text="Contact Us"/>
+
+{/* Hero  End*/}
+
+
+{/* About Us Section Start*/}
+
+<Breaker background='#181c26' twoCol={true} >
+<IntroText 
+eyebrow="About Us"
+  titleH1="We bring business to life" 
+  linkSrc='/' 
+  body='Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.'
+  linkText='Contact Us'
+  componentPosition='left'
+  buttonTopRight={false}
+ primary={true}
+  />
+<StaticImage
+  src="../images/astro.png"
+  width={300}
+  height={500}
+  quality={95}
+  formats={["auto", "webp", "avif"]}
+  alt="A Gatsby astronaut"
+/>
+
+</Breaker>
+
+{/* About Us Section End*/}
+
+
+
+{/* Services Section Start*/}
+<Breaker background='#160f20' twoCol={false} >
+<IntroText 
+eyebrow="Services"
+  titleH1="We do everything" 
+  linkSrc='/' 
+  body='Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.'
+  linkText='See All'
+  componentPosition='left'
+  buttonTopRight={false}
+ primary={true}
+  />
+
+<CardList>
+  <Card 
+  title='Tech Services'
+  body='Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.'
+  />
+  <Card 
+  title='Networking'
+  body='Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.'
+  />
+    <Card 
+  title='Health'
+  body='Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.'
+  />
+
+  </CardList>
+
+
+</Breaker>
+
+{/* Services Section End*/}
+
+
+
+{/* Portfolio  Section Start*/}
+<BreakerLarge isHero={true} background={'#181c26'}>
+  <div className={styles.centerGatsbyImage}>
+  </div>
+  <IntroText 
+eyebrow="Portfolio"
+  titleH1="Some of our finest moments" 
+  linkSrc='/' 
+  body='Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.'
+  linkText='See All'
+  componentPosition='left'
+  buttonTopRight={false}
+ primary={true}
+  />
+  
+
+</BreakerLarge>
+
+{/* Portfolio  Section End */}
+
+
+{/* Testimonial  Section Start*/}
+
+<Breaker background='#160f20' twoCol={false} >
+<IntroText 
+eyebrow="Testimonials"
+  titleH1="What clients say" 
+  linkSrc='/' 
+  body='Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.'
+  linkText='See All'
+  componentPosition='left'
+  buttonTopRight={false}
+ primary={true}
+  />
+
+<CardList>
+  <Card 
+  title='Vincent Wasswa'
+  body='Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.'
+  />
+  <Card 
+  title='Vincent Wasswa'
+  body='Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.'
+  />
+
+  </CardList>
+
+
+</Breaker>
+
+{/* Testimonial  Section End */}
+
+
+{/* News  Section Start */}
+<Breaker background='#160f20' twoCol={false} >
+<IntroText 
+eyebrow="News"
+  titleH1="Blog" 
+  linkSrc='/' 
+  body='Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.'
+  linkText='See All'
+  componentPosition='left'
+  buttonTopRight={false}
+ primary={true}
+  />
+
+</Breaker>
+
+{/* News  Section End */}
+
+
+{/* Contact  Section Start */}
+
+<Breaker background='#181c26' twoCol={false} >
+
+<CardList>
+  <Card 
+  title='Say hello'
+  body='Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.'
+  />
+  <Card 
+  title='Contact Info'
+  body='Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.'
+  />
+
+  </CardList>
+
+
+</Breaker>
+{/* Contact  Section End */}
   
   </Layout>
 )

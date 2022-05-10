9b9eb3f862f968443510faa8dc031ec8bc8c31b7
Matthew Hood <matthewhood@Matthews-MBP-3.lan>
2022-05-10 16:02:31 -0400

    updating placeholder text

--- src/components/Card/card.js
+++ src/components/Card/card.js
@@ -1,52 +1,84 @@
-import React from 'react'
-import ReactDOM from 'react-dom';
-import StarRatingComponent from 'react-star-rating-component';
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import LanguageIcon from '@mui/icons-material/Language';
-import * as styles from './style.module.scss';
+import React from "react"
+import ReactDOM from "react-dom"
+import StarRatingComponent from "react-star-rating-component"
+import Box from "@mui/material/Box"
+import Card from "@mui/material/Card"
+import CardActions from "@mui/material/CardActions"
+import CardContent from "@mui/material/CardContent"
+import Button from "@mui/material/Button"
+import Typography from "@mui/material/Typography"
+import LanguageIcon from "@mui/icons-material/Language"
+import * as styles from "./style.module.scss"
 
-
-const card = (props) => {
+const card = props => {
   const {
-    eyebrow, logo, title, titleH2, body, componentPosition, textAlignment = 'left', logoPosition, logoName, logoNameAttribution, primary, secondary, showRating, background = '#14153b'
+    eyebrow,
+    logo,
+    title,
+    titleH2,
+    body,
+    componentPosition,
+    textAlignment = "left",
+    logoPosition,
+    logoName,
+    logoNameAttribution,
+    primary,
+    secondary,
+    showRating,
+    background = "#14153b",
   } = props
 
-  const cardBackgroundColor = secondary ? 'linear-gradient(.25turn, #e62a7c , 70%, #6a2c90)' : '#14153b'
+  const cardBackgroundColor = secondary
+    ? "linear-gradient(.25turn, #e62a7c , 70%, #6a2c90)"
+    : "#14153b"
   return (
-    <Card className={styles.card} sx={{ minWidth: 275 , background: `${cardBackgroundColor}`, textAlign: `${textAlignment}`, position: 'relative'}}>
-    <CardContent>
-{
-  textAlignment === 'center' ? <LanguageIcon sx={{ position: 'absolute', top: '2rem', backgroundColor: '#fff', marginLeft: 'unset', color: '#000', borderRadius: '6px'}}/> : 
-  <LanguageIcon sx={{ marginLeft: '.7rem'}}/>
-}
-   
-      <Typography variant="h5" component="div"  sx={{ padding: '1rem 0', fontWeight: 'bold', paddingTop: '1.5rem', fontWeight: '900' }}>
-        {title}
-      </Typography >
-      <Typography variant="body2">
-       {body}
-      </Typography>
-      <p   style={{ marginBottom: "-5rem", marginTop: "2rem"}}>
-      {
-  showRating ? <StarRatingComponent 
-  name="rate1" 
-  starCount={5}
-  value={4}
+    <Card
+      className={styles.card}
+      sx={{
+        minWidth: 275,
+        background: `${cardBackgroundColor}`,
+        textAlign: `${textAlignment}`,
+        position: "relative",
+      }}
+    >
+      <CardContent>
+        {textAlignment === "center" ? (
+          <LanguageIcon
+            sx={{
+              position: "absolute",
+              top: "2rem",
+              backgroundColor: "#fff",
+              marginLeft: "unset",
+              color: "#000",
+              borderRadius: "6px",
+            }}
+          />
+        ) : (
+          <LanguageIcon sx={{ marginLeft: ".7rem" }} />
+        )}
 
-/> : ''
-}
-      </p>
-  
-    </CardContent>
-
-   
-  </Card>
-    
+        <Typography
+          variant="h5"
+          component="div"
+          sx={{
+            padding: "1rem 0",
+            fontWeight: "bold",
+            paddingTop: "1.5rem",
+            fontWeight: "900",
+          }}
+        >
+          {title}
+        </Typography>
+        <Typography variant="body2">{body}</Typography>
+        <p style={{ marginBottom: "-5rem", marginTop: "2rem" }}>
+          {showRating ? (
+            <StarRatingComponent name="rate1" starCount={5} value={4} />
+          ) : (
+            ""
+          )}
+        </p>
+      </CardContent>
+    </Card>
   )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import * as React from "react"
 import { StaticImage } from "gatsby-plugin-image"
-import * as styles from './style.module.scss';
+import * as styles from "./style.module.scss"
 import Card from "../components/Card/card"
 import Layout from "../components/Layout/layout"
 import Seo from "../components/seo"
@@ -8,242 +8,237 @@ import IntroText from "../components/IntroText/introText"
 import BreakerLarge from "../components/BreakerLarge/breakerLarge"
 import Breaker from "../components/Breaker/breaker"
 import CardList from "../components/CardList/cardList"
-import MasonaryGrid from "../components/MasonaryGrid/masonaryGrid";
-import BlogGrid from "../components/BlogGrid/blogGrid";
+import MasonaryGrid from "../components/MasonaryGrid/masonaryGrid"
+import BlogGrid from "../components/BlogGrid/blogGrid"
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
 
     {/* Hero  Start*/}
-<BreakerLarge isHero={true} background={'#160f20'}>
-  <div className={styles.centerGatsbyImage}>
-  <StaticImage
-  src="../images/wave.png"
-  width={300}
-  height={500}
-  quality={95}
-  formats={["auto", "webp", "avif"]}
-  alt="A Gatsby astronaut"
-  style={{  filter: "drop-shadow(30px 10px 4px black)", transform: "scale(.9)", margin: "3rem 0", opacity: ".4", marginBottom: '4rem' }}
- 
-/>
-  </div>
+    <BreakerLarge isHero={true} background={"#160f20"}>
+      <div className={styles.centerGatsbyImage}>
+        <StaticImage
+          src="../images/wave.png"
+          width={300}
+          height={500}
+          quality={95}
+          formats={["auto", "webp", "avif"]}
+          alt="A Gatsby astronaut"
+          style={{
+            filter: "drop-shadow(30px 10px 4px black)",
+            transform: "scale(.9)",
+            margin: "3rem 0",
+            opacity: ".4",
+            marginBottom: "4rem",
+          }}
+        />
+      </div>
 
-  <IntroText 
-  titleH1="Digital Agency" 
-  linkSrc='/' 
-  body='Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.'
-  linkText='Contact Us'
-  componentAlignment='center'
-  buttonTopRight={false}
-  textAlignment='center'
-  componentAlignment='center'
-  />
-  <CardList>
-  <Card 
-  title='Feature Concepts'
-  body='Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.'
-  />
-  <Card 
-  title='Big Ideas'
-  body='Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.'
-  secondary={true}
-  />
-    <Card 
-  title='Creative'
-  body='Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.'
-  />
+      <IntroText
+        titleH1="Innovative Solutions"
+        linkSrc="/"
+        body="To make a difference and impact lives, requires a team effort. That is our commitment. To provide solutions to help you solve problems in order to achieve the mission of your business."
+        linkText="Contact Us"
+        componentAlignment="center"
+        buttonTopRight={false}
+        textAlignment="center"
+      />
+      <CardList>
+        <Card
+          title="Feature Concepts"
+          body="Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus."
+        />
+        <Card
+          title="Big Ideas"
+          body="Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus."
+          secondary={true}
+        />
+        <Card
+          title="Creative"
+          body="Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus."
+        />
+      </CardList>
+    </BreakerLarge>
 
-  </CardList>
+    {/* Hero  End*/}
 
-</BreakerLarge>
+    {/* About Us Section Start*/}
 
-{/* Hero  End*/}
+    <Breaker background="#181c26" twoCol={true}>
+      <IntroText
+        eyebrow="About Us"
+        titleH2="We bring business to life"
+        linkSrc="/"
+        body="Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus."
+        linkText="Contact Us"
+        componentPosition="left"
+        buttonTopRight={false}
+        primary={true}
+        textAlignment="left"
+        componentAlignment="left"
+      />
+      <div>
+        <StaticImage
+          src="../images/astro.png"
+          width={300}
+          height={500}
+          quality={95}
+          formats={["auto", "webp", "avif"]}
+          alt="A Gatsby astronaut"
+          className={styles.hideOnMobile}
+        />
+        <StaticImage
+          src="../images/wave.png"
+          width={300}
+          height={500}
+          quality={95}
+          formats={["auto", "webp", "avif"]}
+          alt="A Gatsby astronaut"
+          style={{
+            transform: "scale(.5)",
+            position: "absolute",
+            right: "0",
+            bottom: "-1rem",
+          }}
+          className={styles.hideOnMobile}
+        />
+      </div>
+    </Breaker>
 
+    {/* About Us Section End*/}
 
-{/* About Us Section Start*/}
+    {/* Services Section Start*/}
+    <Breaker background="#160f20" twoCol={false}>
+      <IntroText
+        eyebrow="Services"
+        titleH2="We do everything"
+        linkSrc="/"
+        body="Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus."
+        linkText="See All"
+        componentPosition="left"
+        buttonTopRight={false}
+        primary={true}
+        textAlignment="left"
+        componentAlignment="left"
+        buttonTopRight={true}
+      />
 
-<Breaker background='#181c26' twoCol={true} >
-<IntroText 
-eyebrow="About Us"
-  titleH2="We bring business to life" 
-  linkSrc='/' 
-  body='Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.'
-  linkText='Contact Us'
-  componentPosition='left'
-  buttonTopRight={false}
- primary={true}
- textAlignment='left'
- componentAlignment='left'
-  />
-  <div>
-  <StaticImage
-  src="../images/astro.png"
-  width={300}
-  height={500}
-  quality={95}
-  formats={["auto", "webp", "avif"]}
-  alt="A Gatsby astronaut"
-  className={styles.hideOnMobile}
-/>
-<StaticImage
-  src="../images/wave.png"
-  width={300}
-  height={500}
-  quality={95}
-  formats={["auto", "webp", "avif"]}
-  alt="A Gatsby astronaut"
-  style={{ transform: "scale(.5)", position: 'absolute', right: '0', bottom: '-1rem'}}
-  className={styles.hideOnMobile}
-/>
+      <CardList>
+        <Card
+          title="Tech Services"
+          body="Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus."
+          textAlignment="center"
+        />
+        <Card
+          title="Networking"
+          body="Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus."
+          secondary={true}
+          textAlignment="center"
+        />
+        <Card
+          title="Health"
+          body="Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus."
+          textAlignment="center"
+        />
+      </CardList>
+    </Breaker>
 
-  </div>
-</Breaker>
+    {/* Services Section End*/}
 
-{/* About Us Section End*/}
+    {/* Portfolio  Section Start*/}
+    <BreakerLarge isHero={true} background={"#181c26"}>
+      <div className={styles.centerGatsbyImage}></div>
+      <IntroText
+        eyebrow="Portfolio"
+        titleH2="Some of our finest moments"
+        linkSrc="/"
+        body="Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus."
+        linkText="See All"
+        componentPosition="left"
+        buttonTopRight={false}
+        primary={true}
+        buttonTopRight={true}
+      />
+      <MasonaryGrid />
+    </BreakerLarge>
 
+    {/* Portfolio  Section End */}
 
+    {/* Testimonial  Section Start*/}
 
-{/* Services Section Start*/}
-<Breaker background='#160f20' twoCol={false} >
-<IntroText 
-eyebrow="Services"
-  titleH2="We do everything" 
-  linkSrc='/' 
-  body='Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.'
-  linkText='See All'
-  componentPosition='left'
-  buttonTopRight={false}
- primary={true}
- textAlignment='left'
- componentAlignment='left'
- buttonTopRight={true}
-  />
+    <Breaker background="#160f20" twoCol={false}>
+      <IntroText
+        eyebrow="Testimonials"
+        titleH2="What clients say"
+        linkSrc="/"
+        body="Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus."
+        linkText="See More"
+        componentPosition="left"
+        buttonTopRight={false}
+        primary={true}
+        buttonTopRight={true}
+      />
 
-<CardList>
-  <Card 
-  title='Tech Services'
-  body='Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.'
-  textAlignment='center'
-  />
-  <Card 
-  title='Networking'
-  body='Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.'
-  secondary={true}
-  textAlignment='center'
-  />
-    <Card 
-  title='Health'
-  body='Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.'
-  textAlignment='center'
-  />
-  </CardList>
-</Breaker>
+      <CardList>
+        <Card
+          title="Vincent Wasswa"
+          body="Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus."
+          showRating={true}
+          secondary={true}
+        />
+        <Card
+          title="Vincent Wasswa"
+          body="Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus."
+          showRating={true}
+          secondary={true}
+        />
+      </CardList>
+    </Breaker>
 
-{/* Services Section End*/}
+    {/* Testimonial  Section End */}
 
+    {/* News  Section Start */}
+    <Breaker background="#160f20" twoCol={false}>
+      <IntroText
+        eyebrow="News"
+        titleH2="Blog"
+        linkSrc="/"
+        body="Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus."
+        linkText="See All"
+        componentPosition="left"
+        buttonTopRight={false}
+        primary={true}
+        buttonTopRight={true}
+      />
+      <BlogGrid />
+    </Breaker>
 
+    {/* News  Section End */}
 
-{/* Portfolio  Section Start*/}
-<BreakerLarge isHero={true} background={'#181c26'}>
-  <div className={styles.centerGatsbyImage}>
-  </div>
-  <IntroText 
-eyebrow="Portfolio"
-  titleH2="Some of our finest moments" 
-  linkSrc='/' 
-  body='Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.'
-  linkText='See All'
-  componentPosition='left'
-  buttonTopRight={false}
- primary={true}
- buttonTopRight={true}
-  />
-  <MasonaryGrid/>
-</BreakerLarge>
+    {/* Contact  Section Start */}
 
-{/* Portfolio  Section End */}
-
-
-{/* Testimonial  Section Start*/}
-
-<Breaker background='#160f20' twoCol={false} >
-<IntroText 
-eyebrow="Testimonials"
-  titleH2="What clients say" 
-  linkSrc='/' 
-  body='Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.'
-  linkText='See More'
-  componentPosition='left'
-  buttonTopRight={false}
- primary={true}
- buttonTopRight={true}
-  />
-
-<CardList>
-  <Card 
-  title='Vincent Wasswa'
-  body='Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.'
-  showRating={true}
-  secondary={true}
-  />
-  <Card 
-  title='Vincent Wasswa'
-  body='Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.'
-  showRating={true}
-  secondary={true}
-  />
-  </CardList>
-</Breaker>
-
-{/* Testimonial  Section End */}
-
-
-{/* News  Section Start */}
-<Breaker background='#160f20' twoCol={false} >
-<IntroText 
-eyebrow="News"
-  titleH2="Blog" 
-  linkSrc='/' 
-  body='Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.'
-  linkText='See All'
-  componentPosition='left'
-  buttonTopRight={false}
- primary={true}
- buttonTopRight={true}
-  />
-  <BlogGrid/>
-</Breaker>
-
-{/* News  Section End */}
-
-
-{/* Contact  Section Start */}
-
-<Breaker background='#181c26' twoCol={true} >
-<IntroText 
-
-  titleH2="Say Hello" 
-  linkSrc='/' 
-  body='Tel: +1(845) 391-1805. Email: info@neverendingsolutions.com'
-  linkText='Contact Us'
-  componentPosition='left'
-  buttonTopRight={false}
- primary={true}
-  />
-    <IntroText 
-  titleH2="Contact Info" 
-  linkSrc='/' 
-  linkText='View Map'
-  body='201 Frozen Ridge Road. Newburgh NY. 12550'
-  componentPosition='left'
-  buttonTopRight={false}
- primary={true}
-  />
-</Breaker>
-{/* Contact  Section End */}
-  
+    <Breaker background="#181c26" twoCol={true}>
+      <IntroText
+        titleH2="Say Hello"
+        linkSrc="/"
+        body="Tel: +1(845) 391-1805. Email: info@neverendingsolutions.com"
+        linkText="Contact Us"
+        componentPosition="left"
+        buttonTopRight={false}
+        primary={true}
+      />
+      <IntroText
+        titleH2="Contact Info"
+        linkSrc="/"
+        linkText="View Map"
+        body="201 Frozen Ridge Road. Newburgh NY. 12550"
+        componentPosition="left"
+        buttonTopRight={false}
+        primary={true}
+      />
+    </Breaker>
+    {/* Contact  Section End */}
   </Layout>
 )
 
